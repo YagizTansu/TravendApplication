6864aab81f48a771594fa5a79b959120191d88df
Sarp <sarpalisaygi@gmail.com>
2021-06-13 16:23:38 +0300

    Sarp

--- app/src/main/java/com/example/travend/MainActivity.java
+++ app/src/main/java/com/example/travend/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //travend for sarp
+        //travend for push
+
+        System.out.println("OL ARTIK");
     }
 }
