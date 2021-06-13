d7c166e9ddce49aaad6fa0c42a234bd28d4bb6a7
yagiz <yagiz.tansu99@gmail.com>
2021-06-13 16:21:05 +0300

    yağız

--- app/src/main/java/com/example/travend/MainActivity.java
+++ app/src/main/java/com/example/travend/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //travend for push
+        //travend for sarp
     }
 }
