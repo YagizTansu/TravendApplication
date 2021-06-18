8e4642325e17b8a3944f37a3f461ff749dde5089
yagiz <yagiz.tansu99@gmail.com>
2021-06-18 22:36:51 +0300

    yağız

--- app/src/main/java/com/example/travend/ProfileFragment.java
+++ app/src/main/java/com/example/travend/ProfileFragment.java
@@ -61,6 +61,4 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
-
-
 }
